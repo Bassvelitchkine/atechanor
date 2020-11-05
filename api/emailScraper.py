90d68465a7bf43247289c1c280f6e362054faf1a
Bastiche <bastien.velitchkine@live.fr>
2020-11-05 22:54:51 +0100

    Successful import and hello world of bs4

--- api/emailScraper.py
+++ api/emailScraper.py
@@ -4,10 +4,14 @@ def emailScraper(url):
     import time
     import requests
     import re
+    from bs4 import BeautifulSoup
 
     urlPortion = re.search('\d+', url)[0]
+
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    title = soup.title.text
 
     res = requests.put('http://web:5005/update/' +
                        urlPortion + '/bob@gmail.com')
 
-    return 'Job Result'
+    return title
