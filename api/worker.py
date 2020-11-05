37d136e5c0ccbea665413b5602d419981690b7be
Bastiche <bastien.velitchkine@live.fr>
2020-11-05 21:53:38 +0100

    Minor modifications

--- api/worker.py
+++ api/worker.py
@@ -3,7 +3,6 @@ import redis
 from rq import Worker, Queue, Connection
 
 listen = ['default']
-# redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url("redis://redis:6379")
 
