8c25aab9c44aa6e7c735728f605e54008644a93e
Bastiche <bastien.velitchkine@live.fr>
2020-11-05 22:42:35 +0100

    IP address replaced by service name

--- api/emailScraper.py
+++ api/emailScraper.py
@@ -7,7 +7,7 @@ def emailScraper(url):
 
     urlPortion = re.search('\d+', url)[0]
 
-    res = requests.put('http://192.168.99.100:5000/update/' +
+    res = requests.put('http://web:5005/update/' +
                        urlPortion + '/bob@gmail.com')
 
     return 'Job Result'
